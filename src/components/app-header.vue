bbc6711a82b123cdfc7d6a4dc775f395b2685ef6
Mahmoud Ahmed <buttererror@gmail.com>
2023-01-01 02:12:44 +0200

    add account avatar and notification icon to the header

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -1,8 +1,15 @@
 <template>
   <v-app-bar app color="#fff" flat outlined extension-height="20">
-    <v-app-bar-nav-icon v-if="drawer === false"
-      @click="drawer = !drawer">
+    <v-app-bar-nav-icon v-if="drawer === false" @click="drawer = !drawer">
     </v-app-bar-nav-icon>
+    <v-spacer></v-spacer>
+    <v-badge bordered top color="red accent-4" dot offset-x="10" offset-y="10">
+      <v-icon> mdi-bell </v-icon>
+    </v-badge>
+    <div class="mx-3"></div>
+    <v-avatar color="indigo" size="40">
+      <v-icon dark> mdi-account-circle </v-icon>
+    </v-avatar>
     <template slot="extension">
       <v-divider></v-divider>
     </template>
